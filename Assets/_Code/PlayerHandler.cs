9ae238b9b497453fb7a53719d9aefb67658eb039
IKOMMM <mikolaj.musialik.media@gmail.com>
2021-07-08 22:58:11 +0200

    Upgrade To Multitouch and to build

--- Assets/_Code/PlayerHandler.cs
+++ Assets/_Code/PlayerHandler.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class PlayerHandler : MonoBehaviour
 {
@@ -23,12 +25,21 @@ public class PlayerHandler : MonoBehaviour
         SpawnNewBall();
     }
 
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
     // Read Coordinates Over Screen
     void Update()
     {
         if(currentBallRigidbody == null) { return; }
 
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        if (Touch.activeTouches.Count == 0)
         {
             if (isDragging)
             {
@@ -43,7 +54,15 @@ public class PlayerHandler : MonoBehaviour
         isDragging = true;
         currentBallRigidbody.isKinematic = true;
 
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        Vector2 touchPosition = new Vector2();
+
+        foreach(Touch touch in Touch.activeTouches)
+        {
+            touchPosition += touch.screenPosition;
+        }
+
+        touchPosition /= Touch.activeTouches.Count;
+
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
